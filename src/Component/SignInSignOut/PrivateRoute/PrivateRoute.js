d78d0c72d4ace19eb06bf44f69fa597635e44828
Md Asaduzaman <asadasad9282@gmail.com>
2022-05-02 16:46:59 +0600

    test

--- src/Component/SignInSignOut/PrivateRoute/PrivateRoute.js
+++ src/Component/SignInSignOut/PrivateRoute/PrivateRoute.js
@@ -10,7 +10,7 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation()
 
     if (loading) {
-        return <Loading></Loading>
+        // return <Loading></Loading> 
     }
 
     if (!user) {
